7f5185a5c76ca4dec41b84ff682be02258f2d5d4
Wagner <wpassarella@gmail.com>
2019-12-27 23:34:52 +0100

    add sg

--- instance.tf
+++ instance.tf
@@ -2,9 +2,12 @@ resource "aws_instance" "test" {
   ami           = "ami-0cc0a36f626a4fdf5"
   instance_type = "t2.micro"
   key_name = "main"
-  vpc_security_group_ids = ["sg-a3f8edc6"]
+  vpc_security_group_ids = ["${aws_security_group.allow_ssh.id}"]
  
   tags = {
     Name = "test-terraform"
   }
 }
+
+
+
---
+++ sg.tf
@@ -0,0 +1,22 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow ssh inbound traffic"
+  vpc_id      = "vpc-1d33f777"
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    # Please restrict your ingress to only necessary IPs and ports.
+    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+  }
+}
